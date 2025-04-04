f6c8feab5253fa8fcc6dfce7cf6eadaf48c5ed52
Álvaro Condorí <condorialvaro95@gmail.com>
2025-04-04 16:56:37 -0300

    El objeto props y children

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,12 +1,7 @@
 import {JSX} from "react";
 
-// generate a random number between 1 and 123
-const random = () => Math.floor(Math.random() * 123) + 1;
+type Props = {image: string};
 
-export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
-
-  return (
-    <img width={320} height='auto' src={image} alt='' className='rounded' />
-  );
+export const RandomFox = ({image}: Props): JSX.Element => {
+  return <img width={320} height='auto' src={image} className='rounded' />;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type {NextPage} from "next";
 import Head from "next/head";
 import {RandomFox} from "../components/RandomFox";
 
+const random = () => Math.floor(Math.random() * 123) + 1;
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -13,7 +15,7 @@ const Home: NextPage = () => {
 
       <main>
         <h1 className='text-3xl font-bold underline'>Hello, Platzi</h1>
-        <RandomFox />
+        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
       </main>
 
       <footer></footer>
